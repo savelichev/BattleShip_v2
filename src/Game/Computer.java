642cc344955e46115299a7a59df36c061e59b12a
Nikolay Savelichev <savelichev.n@gmail.com>
2016-05-17 01:10:22 +0300

    add classes Human,Computer

---
+++ src/Game/Computer.java
@@ -0,0 +1,18 @@
+package Game;
+
+import Field.Cell;
+
+/**
+ * Created by savel_000 on 17.05.2016.
+ */
+public class Computer extends Player {
+    @Override
+    public Cell move() {
+        return null;
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+}
---
+++ src/Game/Human.java
@@ -0,0 +1,18 @@
+package Game;
+
+import Field.Cell;
+
+/**
+ * Created by savel_000 on 17.05.2016.
+ */
+public class Human extends Player {
+    @Override
+    public Cell move() {
+        return null;
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+}
